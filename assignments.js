cbfe45378701fcfc95f30f705a8d2051a593a79d
Gerson Valdivia <gerav94@yahoo.com>
2025-12-28 12:06:55 -0600

    Data Types assignment

--- assignments.js
+++ assignments.js
@@ -1,7 +1,11 @@
 let country = "Nicaragua"
 let continent = "America"
 let population = 2000000
+let isIsland = false
+let language
 
+console.log(isIsland)
 console.log(country)
 console.log(continent)
 console.log(population)
+console.log(language)
